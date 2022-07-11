3dab87761e1e1a14cc7a94e2efe914a9849f8456
suyeonsu <agew0423@daum.net>
2022-07-11 23:04:56 +0900

    React-Router-Dom

---
+++ client/src/components/views/LoginPage/LoginPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function LoginPage() {
+    return (
+        <div>
+            LoginPage 로그인페이지
+        </div>
+    )
+}
+
+export default LoginPage
---
+++ client/src/components/views/RegisterPage/RegisterPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function RegisterPage() {
+    return (
+        <div>
+            RegisterPage 회원가입페이지
+        </div>
+    )
+}
+
+export default RegisterPage
